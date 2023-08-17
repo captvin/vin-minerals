d03826d618839ce47e1041d290154f815cc7770a
captvin <alpinoke82@gmail.com>
2023-08-17 21:35:34 +0700

    update environment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,11 @@
+
 // import getLocalStorage dari helper/localStorage yang sudah dibuat
 import { getLocalStorage } from "./helper/localStorage";
 
 // buat constant untuk menyimpan url server
-const BASE_API = "http://192.168.0.4:8085";
+const BASE_API = import.meta.env.VITE_API_URL;
 // buat constant untuk menyimpan url server untuk image
-const BASE_API_IMAGE = "http://localhost:8080/image";
+// const BASE_API_IMAGE = "http://localhost:8080/image";
 
 // buat constant untuk menyimpan key local storage
 const LOCAL_STORAGE_TOKEN = "wikusamaCafe/token";
@@ -20,7 +21,7 @@ const TOKEN = {
 // export constant
 export {
   BASE_API,
-  BASE_API_IMAGE,
+  // BASE_API_IMAGE,
   LOCAL_STORAGE_TOKEN,
   LOCAL_STORAGE_USER,
   TOKEN,
